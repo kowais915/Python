6dd0e6c35e6de515f5cab7cddd21433705683b55
Owais Khan <45107209+kowais915@users.noreply.github.com>
2020-05-07 20:19:53 +0500

    Create 100th_year_calculator.py

---
+++ 100th_year_calculator.py
@@ -0,0 +1,6 @@
+name =input("Please enter your name: ")
+age = input("Please enter your age: ")
+
+#22 + x =100, x =100+22, 2020 + x
+year = 2020 + (100 - int(age))
+print(f'{name}, You will turn 100 in year {year}')
