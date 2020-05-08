31edb3bb6ae8a99c6188e4595700f8dfc414d763
Owais Khan <45107209+kowais915@users.noreply.github.com>
2020-05-08 23:44:36 +0500

    Update 100th_year_calculator.py

--- 100th_year_calculator.py
+++ 100th_year_calculator.py
@@ -1,6 +1,8 @@
+#This program simply calculates the year when the user will turn 100
 name =input("Please enter your name: ")
 age = input("Please enter your age: ")
 
-#22 + x =100, x =100+22, 2020 + x
+#Here is how you calculate the year 22 + x =100, x =100+22, 2020 + x
+
 year = 2020 + (100 - int(age))
 print(f'{name}, You will turn 100 in year {year}')
